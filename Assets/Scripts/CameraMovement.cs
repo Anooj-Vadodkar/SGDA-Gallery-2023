fc727a2b70b7e3d90f1f5cf9dd5c7b3b1a53c21b
Anooj Vadodkar <anoojrv@gmail.com>
2023-06-13 13:20:33 -0700

    Interaction touched up
    The UI now shows up when you're in front of a specified object and you press e; it also turns off when you either leave the range or press e again

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensX = 100f;
+    public float sensY = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    public float interactRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
